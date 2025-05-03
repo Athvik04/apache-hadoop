15df1e0cc3935fc155a6d1771888279eda853dcd
Athvik <raoathvik@gmail.com>
2025-05-03 17:36:16 +0530

    3,4,5

--- WordCount/WC_Reducer.java
+++ WordCount/WC_Reducer.java
@@ -1,11 +1,12 @@
 import java.io.IOException;     
-import java.util.Iterator;     
+import java.util.Iterator;     //
 import org.apache.hadoop.io.IntWritable;     
 import org.apache.hadoop.io.Text;     
 import org.apache.hadoop.mapred.MapReduceBase;     
+import org.apache.hadoop.mapred.Reducer;      //  
 import org.apache.hadoop.mapred.OutputCollector;     
-import org.apache.hadoop.mapred.Reducer;     
 import org.apache.hadoop.mapred.Reporter;     
+
 public class WC_Reducer  extends MapReduceBase implements 
 Reducer<Text,IntWritable,Text,IntWritable>  
 {     
